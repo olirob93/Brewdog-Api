7863e7e09a51daebb1d43727e9bf2c48d9d22459
Ollie Robins <ollierobins93@gmail.com>
2020-10-27 21:35:50 +0000

    added firebase and styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,24 +3,24 @@ import styles from "./Card.module.scss";
 
 const Card = (props) => {
   
-const {name, image_url, description, abv ,ibu, first_brewed} = props.beerData;
- 
-  return (
-    <>
-      <div className={styles.card}>
-        <img src={image_url} alt="image of beer"/>
-        <div className={styles.cardData}>
-          <h1>{name}</h1>
-          <p>{description}</p>
-          <div>
-            <li>ABV: {abv}</li>
-            <li>IBU: {ibu}</li>
-            <li>First Brewed: {first_brewed}</li>
+  const {name, image_url, description, abv ,ibu, first_brewed} = props.beerData;
+  
+    return (
+      <>
+        <div className={styles.card}>
+          <img src={image_url} alt="image of beer"/>
+          <div className={styles.cardData}>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <div>
+              <li>ABV: {abv}</li>
+              <li>IBU: {ibu}</li>
+              <li>First Brewed: {first_brewed}</li>
+            </div>
           </div>
         </div>
-        </div>
-    </>
-  );
+      </>
+    );
 };
 
 export default Card;
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,18 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import styles from "./CardList.module.scss";
 import Card from '../Card';
-import image from '../../assets/images/alban-martel-nhX8QhXMBkM-unsplash.jpg';
 
 const CardList = (props) => {
 
-const getJsxContent = (beers) => (<Card key={beers.id} beerData={beers}/>) 
-  
-//const getJsxContent = (beers) => beers.length ? (<Card key={beers.id} beerData={beers}/>) :
-//(<p>no beers available</p>)
-
+  const getJsxContent = (beers) => (<Card key={beers.id} beerData={beers}/>) 
 
   return (
-   <section className={styles.cardList}> {props.filteredBeers.map(getJsxContent)}  </section>
+  <section className={styles.cardList}> {props.filteredBeers.map(getJsxContent)}  </section>
   )
 };
 
